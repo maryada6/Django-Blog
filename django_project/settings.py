5293dc06e4007a3830cb0d7159525f0e4d5c0fe4
maryada 6 <maryada@student.tce.edu>
2021-07-01 18:10:50 +0530

    working

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +133,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER=env('EMAIL_USER')
+EMAIL_HOST_PASSWORD=env('EMAIL_PASS')
